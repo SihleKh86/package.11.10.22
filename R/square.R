31d4e8eb7e5862d35cf3507dab1e66cb6760c7f1
sihlechem@gmail.com <sihlek@umich.edu>
2022-11-10 16:59:04 -0500

    add vignetter

---
+++ R/square.R
@@ -0,0 +1,18 @@
+#'Square
+#'
+#'Gets the square of a member
+#'
+#'@param x input value
+#'
+#'@return the square of x
+#'
+#'@return the square of x
+#'
+#'@examples
+#'square(3)
+#'
+#'@export
+#'
+square = function(x) {
+  return (x^2)
+}
